2f8b43ea6c8d43c481c0e136526e2defd9ca5251
iBabs <ibrahimbabalola8@gmail.com>
2024-11-25 12:50:08 +0100

    feat: update API endpoints to use environment variables and add .env to .gitignore

--- src/pages/auth/Register.jsx
+++ src/pages/auth/Register.jsx
@@ -28,7 +28,7 @@ const handleSubmit =async(e)=>{
   e.preventDefault()
   try{
     console.log(formData)
-    const res = await axios.post("http://localhost:7788/api/v1/auth/signup", formData)
+    const res = await axios.post(`${import.meta.env.VITE_API_URL}/api/v1/auth/signup`, formData)
     console.log(res.data)
     localStorage.setItem("user", JSON.stringify(res.data))
     toast.success("Registration successful 👍")
